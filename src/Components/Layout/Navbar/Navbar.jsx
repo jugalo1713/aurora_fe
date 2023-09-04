f462e04e713d4ca273cbf28830b83a6726feb0ef
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-09-03 19:13:50 -0500

    test

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -75,6 +75,7 @@ const Navbar = () => {
                         </MenuItem>
                       ))}
                     </Menu>
+
                   </Tooltip>
                 </>
                 :
