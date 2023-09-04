35528e2dc3cc034d8587f68ec44e2fb62c8aedd4
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-09-04 09:35:42 -0500

    redirect added to routes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -29,6 +29,13 @@ const handler = NextAuth({
             session.user.id = token.id
 
             return session
+        },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
         }
     }
 });
