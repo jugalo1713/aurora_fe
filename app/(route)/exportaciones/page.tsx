3932e01ad0d2c7c9ac04febc8ae1bb46de578e4b
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2024-01-19 16:57:05 -0500

    pages created

---
+++ app/(route)/exportaciones/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const page = () => {
+  return <main className=" ">exportaciones</main>;
+};
+
+export default page;
---
+++ app/(route)/terrestres/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Terrestres</div>;
+};
+
+export default Page;
