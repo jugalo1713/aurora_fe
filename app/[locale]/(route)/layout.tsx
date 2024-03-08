1e32b0b7a11a39dd89e17da92aa1f20e165e77d3
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2024-03-08 16:51:52 -0500

    AUR-9 kinde initial setup

--- app/[locale]/(route)/layout.tsx
+++ app/[locale]/(route)/layout.tsx
@@ -3,11 +3,20 @@ import NavigationBar from "@/components/shared/Navigation/NavigationBar";
 import Theme from "@/components/shared/Theme";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import React from "react";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider>
       <div className="flex flex-row-reverse mt-3 gap-8 items-center">
+        <LoginLink>Sign in</LoginLink>
+        <RegisterLink>Sign up</RegisterLink>
+        <LogoutLink>Logout</LogoutLink>
+
         <Theme />
         <LocaleSwitcher />
       </div>
